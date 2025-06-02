f5cc45b55a81f378f4009885bc1886d10e235369
Amir Shakibafar <amir.shakibafar82@gmail.com>
2025-06-03 03:17:02 +0330

    setup github sign in and auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+// app/signin/page.tsx
+
+export default function SignInPage() {
+  return (
+    <main className="p-4">
+      <h1 className="text-xl font-bold mb-4">Home</h1>
+    </main>
+  );
+}
